7d067474a85b4e8ede85e1f5dc5973352e70d008
MRX <sureshmadawa763@gmail.com>
2023-12-23 21:08:26 +0530

    ready to build

--- next.config.js
+++ next.config.js
@@ -1,4 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
+const nextConfig = {
+  trailingSlash: true,
+  output: "export",
+  images: {
+    unoptimized: true,
+  },
+  eslint: {
+    ignoreDuringBuilds: true,
+  },
+};
 
-module.exports = nextConfig
+module.exports = nextConfig;
